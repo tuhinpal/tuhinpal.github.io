5cc4ac2d776baaf02d10bf91617fadb9e01c258b
Tuhin Kanti Pal <me@mailtuhin.ml>
2021-02-19 11:05:27 +0530

    Slightly changes on BIO

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -40,7 +40,7 @@ export default class Index extends React.Component {
             <div className="container">
                 <div>
                     <h1>I'm <span className="geruya">Tuhin</span> <span className="sada">Kanti</span> <span className="sobuj">Pal</span>.</h1>
-                    <p>I'm a programmer, Security Researcher and Opensource Enthusiast. I have Researched about many Indian OTT apps security and founded many bugs as well. I have created some awesome Open Source projects, They are available on my <a target="_blank" href="https://github.com/cachecleanerjeet">Github</a>.
+                    <p>I'm a programmer, Security Researcher and Opensource Enthusiast. I have Researched about many OTTs and founded many bugs. I have created some awesome Open Source projects, They are available on my <a target="_blank" href="https://github.com/cachecleanerjeet">Github</a>.
                   Yeah, I love Monospace.
               </p>
                     <Link to="../../works" className="button">View my Works</Link>
