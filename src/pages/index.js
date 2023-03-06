7fc5d10f75a0722d40670a982de7ab5363c81bf1
tuhinpal <me@thetuhin.com>
2023-03-06 09:21:26 +0530

    Added on hover effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,35 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import me from "../images/me.jpg";
+import meColored from "../images/me-colored.jpg";
 import { Link } from "gatsby";
 
 export default function Index() {
+  const imgRef = React.useRef(null);
+  const [imageIsDotted, setimageIsDotted] = React.useState(true);
+
+  React.useEffect(() => {
+    function imageHoverChecker() {
+      if (!imgRef.current) return;
+
+      imgRef.current.addEventListener("mouseenter", () => {
+        setimageIsDotted(false);
+      });
+
+      imgRef.current.addEventListener("mouseleave", () => {
+        setimageIsDotted(true);
+      });
+    }
+
+    imageHoverChecker();
+
+    return () => {
+      setimageIsDotted(true);
+      document.removeEventListener("mouseenter", imageHoverChecker);
+      document.removeEventListener("mouseleave", imageHoverChecker);
+    };
+  }, []);
+
   return (
     <>
       <title>Tuhin</title>
@@ -70,7 +96,15 @@ export default function Index() {
           </p>
         </div>
         <div className="imagectnr">
-          <img className="img" src={me} alt="Tuhin" />
+          <img
+            className="img"
+            src={imageIsDotted ? me : meColored}
+            alt="Tuhin"
+            ref={imgRef}
+            style={{
+              opacity: imageIsDotted ? 0.6 : 1,
+            }}
+          />
         </div>
       </div>
     </>
