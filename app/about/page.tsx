4bb942b0c0a9b464e111cdc4ff80c199ec29f16d
tuhinpal <me@thetuhin.com>
2023-07-09 13:28:04 +0530

    Little header fixes

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between">
+      hello
+    </div>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
 
   return (
     <header className="flex flex-row justify-between items-center w-full py-8">
-      <div className="items-center gap-3 hidden md:flex">
+      <div className="items-center gap-3 hidden md:flex w-1/3">
         {isAboutPage ? (
           <Anchor className="text-lg" href={"/"}>
             Back to main
@@ -26,11 +26,12 @@ export default function Header() {
         )}
       </div>
 
-      <div>
+      <div className="w-1/3 flex justify-start md:justify-center">
         <Logo width={120} height={30} className="dark:filter dark:invert" />
       </div>
 
       <SocialMedia
+        className="w-1/3 justify-end"
         buttonStyle={{ padding: "0.625rem" }}
         iconStyle={{ width: "1.25rem", height: "1.25rem" }}
       />
