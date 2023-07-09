d016ff9b4057af78d3952e733bb4c13e8c778597
tuhinpal <me@thetuhin.com>
2023-07-09 22:52:07 +0530

    Added image optimization

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -2,10 +2,19 @@ import markdownIt from "markdown-it";
 import { useMemo } from "react";
 
 export default function RenderMarkdown({ content = "", className = "" }) {
-  const __html = useMemo(
-    () => markdownIt({ linkify: true, html: true }).render(content),
-    [content]
-  );
+  const __html = useMemo(() => {
+    const html = markdownIt({ linkify: true, html: true }).render(content);
+    const replaceImageHtml = html.replace(
+      // if imageUrl starts with /images/ then replace it with /_next/image?url={imageUrl}&w=1080&q=75
+      /<img src="\/images\/(.+)" alt="(.+)">/g,
+      (_, imageUrl, imageAlt) =>
+        `<img src="/_next/image?url=${encodeURIComponent(
+          `/images/${imageUrl}`
+        )}&w=1080&q=75" alt="${imageAlt}">`
+    );
+
+    return replaceImageHtml;
+  }, [content]);
 
   return (
     <article
