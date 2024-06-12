db2a5e9307e6834ff2e2f11cd670a7e2cbb2db20
Tuhin Kanti Pal <tkp03463@gmail.com>
2024-06-12 14:03:29 +0000

    Lil

--- app/(site)/components/Hero.tsx
+++ app/(site)/components/Hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col md:flex-row gap-6 md:gap-8 items-start md:items-center mt-16 md:mt-24 w-full md:w-max">
+    <div className="flex flex-col md:flex-row gap-6 md:gap-8 items-start md:items-center mt-16 md:mt-24 w-full md:w-max flex-wrap">
       <div>
         <Image
           src="/images/me-square.jpeg"
@@ -26,9 +26,11 @@ export default function Hero() {
         <span>I build web products.</span>
       </h1>
 
-      <Anchor href="/about" aria-label="About Page">
-        About Me
-      </Anchor>
+      <div className="w-full">
+        <Anchor href="/about" aria-label="About Page" className="py-2.5 text-lg">
+          About Me
+        </Anchor>
+      </div>
     </div>
   );
 }
